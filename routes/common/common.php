2f7284b57a43789bf7e5feae1e25ddc0fbca350b
iLay1678 <807190135@qq.com>
2020-11-05 21:27:27 +0800

    修复路由错误

--- routes/common/common.php
+++ routes/common/common.php
@@ -6,7 +6,7 @@ Route::group(['middleware' => ['switch.language'], 'namespace' => 'Home'], funct
     Route::get('bill/{orderid}', 'OrderController@bill');
     Route::post('postOrder', 'OrderController@createOrder');
     Route::get('getOrderStatus/{orderid}', 'OrderController@getOrderStatus');
-    Route::get('searchOrder', 'OrdersController@searchOrder');
+    Route::get('searchOrder', 'OrderController@searchOrder');
     Route::match(['get', 'post'], 'searchOrderById/{oid?}', 'OrderController@searchOrderById');
     Route::post('searchOrderByAccount', 'OrderController@searchOrderByAccount');
     Route::get('searchOrderByBrowser', 'OrderController@searchOrderByBrowser');
