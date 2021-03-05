e73be543f2a163f3b8e2d76e7de1944ac6dce1cd
我若为王 <807190135@qq.com>
2021-03-05 14:38:12 +0800

    修复商品限购

--- app/Http/Controllers/Home/OrderController.php
+++ app/Http/Controllers/Home/OrderController.php
@@ -115,6 +115,7 @@ class OrderController extends Controller
         $product = $this->productService->product($data['pid']);
         if (empty($product) || $product['pd_status'] != 1) throw new AppException(__('prompt.product_off_the_shelf'));
         if ($product['in_stock'] == 0 || $data['order_number'] > $product['in_stock']) throw new AppException(__('prompt.inventory_shortage'));
+        if($product['buy_limit'] !=0 && $data['order_number'] > $product['buy_limit']) throw new AppException(__('prompt.buy_limit').$product['buy_limit']);
         // 订单缓存
         $cacheOrder = [
             'product_id' => $data['pid'], // 商品id
